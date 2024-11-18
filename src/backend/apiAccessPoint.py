edad9f6a77cf96bec7aeae6dbec4e7c251c84d69
James Ropotar <jamie.ropotar@gmail.com>
2024-11-18 12:31:42 -0800

    Created Access Point for UI to backend

---
+++ src/backend/apiAccessPoint.py
@@ -0,0 +1,31 @@
+
+
+class ApplicationHome():
+    def init_app():
+        #Inventory Manager Initializaiton
+        #WorkOrder List initialization 
+        #Calendar Initialization 
+        #database initialization
+        #initialize global user 
+
+
+
+    def calculateMetrics(metricType):
+        #takes in type of metric as an argument, processes
+        a = 0
+        #returns required functional value
+
+    def calendarFunction(functionType, args[]):
+        a = 0 
+        #returns necessary object 
+
+    def inventoryFunction(functionType, args[]):
+        a = 0
+        #returns necessary object 
+
+    def getWorkOrderList():
+        return workOrderList
+    
+    def userFunctions():
+        a = 0
+        #returns a success case
