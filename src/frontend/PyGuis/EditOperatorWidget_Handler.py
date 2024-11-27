2d6383f461eb3324c35eafc6be60c1be7223efa6
Morgan <morgandavis239@gmail.com>
2024-11-27 14:25:29 -0800

    OperatorManagerWidget_Handler can now open subwidgets (add, edit, remove), EditOperatorWidget_Handler created, INT validation added for employee number in operator widgets (add, edit)

--- src/frontend/PyGuis/EditOperatorWidget_Handler.py
+++ src/frontend/PyGuis/EditOperatorWidget_Handler.py
@@ -1,33 +1,33 @@
-from AddNewOperatorWidget import Ui_addNewOperatorWidget
+from EditOperatorWidget import Ui_editOperatorWidget
 from PyQt5 import QtWidgets as qtw
 # from PyQt5 import QtGui
 from PyQt5 import QtCore as qtc
 
 from PyQt5.QtGui import QIntValidator  # Import the QIntValidator for numeric validation
 
-class AddNewOperatorHandler(qtw.QWidget):
-    
+class EditOperatorWidgetHandler(qtw.QWidget):
+
     def __init__(self):
         super().__init__()
-        self.ui = Ui_addNewOperatorWidget()
+        self.ui = Ui_editOperatorWidget()
         self.ui.setupUi(self)
         self.ui.saveOperatorButton.clicked.connect(self.saveOperatorButtonClicked)
-# Set QIntValidator to restrict input to integers only
+ # Set QIntValidator to restrict input to integers only
         self.ui.newEmployeeNumberInput.setValidator(QIntValidator())  # Accept any integer value
 
     def saveOperatorButtonClicked(self):
         operatorName =  self.ui.newOperatorNameInput.text()
         employeeNumber = self.ui.newEmployeeNumberInput.text()
-    
+
         print("New Operator Name: ")
         print(operatorName)
         print("New Employee Number: ")
         print(employeeNumber)
         self.close()
-
+        pass
 
 if __name__ == '__main__':
     app = qtw.QApplication([])
-    widget = AddNewOperatorHandler()
+    widget = EditOperatorWidgetHandler()
     widget.show()
     app.exec()
