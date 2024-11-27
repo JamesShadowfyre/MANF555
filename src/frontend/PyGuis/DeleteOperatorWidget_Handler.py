e55e72ce8374ad802c22a71b0e86536a87729381
Morgan <morgandavis239@gmail.com>
2024-11-27 13:48:24 -0800

    DeleteOperatorWidget Handler created and working. DeleteOperatorWidget and DeleteInventoryWidget changed to PyQt5 from PyQt6

---
+++ src/frontend/PyGuis/DeleteOperatorWidget_Handler.py
@@ -0,0 +1,24 @@
+from DeleteOperatorWidget import Ui_DeleteOperatorWidget
+from PyQt5 import QtWidgets as qtw
+# from PyQt5 import QtGui
+from PyQt5 import QtCore as qtc
+
+class DeleteOperatorWidgetHandler(qtw.QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_DeleteOperatorWidget()
+        self.ui.setupUi(self)
+        self.ui.deleteOperatorButton.clicked.connect(self.deleteOperatorButtonClicked)
+    
+    def deleteOperatorButtonClicked(self):
+        operatorName =  self.ui.deleteOperatorComboBox.currentText()
+        employeeNumber = self.ui.deleteEmployeeNumberBox.text()
+        self.close()
+
+# Code to launch widget
+if __name__ == '__main__':
+    app = qtw.QApplication([])
+    widget = DeleteOperatorWidgetHandler()
+    widget.show()
+    app.exec()
