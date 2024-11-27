65cdbd96ca68e1f977853181ee7386143a93806c
Morgan <morgandavis239@gmail.com>
2024-11-26 23:47:40 -0800

    KPI handler started, and removed instances of "parent=" from KPI.py

--- src/frontend/PyGuis/KPI_Handler.py
+++ src/frontend/PyGuis/KPI_Handler.py
@@ -1,2 +1,19 @@
 from KPI import Ui_Form
 
+from PyQt5 import QtWidgets as qtw
+# from PyQt5 import QtGui
+from PyQt5 import QtCore as qtc
+
+class KPIHandler(qtw.QWidget): 
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+
+#Code to launch widget
+if __name__ == '__main__':
+    app = qtw.QApplication([])
+    widget = KPIHandler()
+    widget.show()
+    app.exec()
