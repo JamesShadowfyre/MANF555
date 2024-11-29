801f723f41d7d70ebb234a8836584dff35b8a409
Morgan <morgandavis239@gmail.com>
2024-11-29 15:56:18 -0800

    Handlers created and edited, ExecutedProduction.ui variables fixed

--- src/frontend/PyGuis/ExecuteProductionWidget_Handler.py
+++ src/frontend/PyGuis/ExecuteProductionWidget_Handler.py
@@ -10,7 +10,28 @@ class ExecuteProductionWidgetHandler(qtw.QWidget):
         self.ui = Ui_ExecuteProduction()
         self.ui.setupUi(self)
 
+        self.ui.executeProductionButton.clicked.connect(self.executeProductionButtonClicked)
         
+    def executeProductionButtonClicked(self):
+        workOrderNumber = self.ui.executeWONumberComboBox.currentText()
+        customer = self.ui.executeWOCustomerSelection.currentText()
+        orderDate = self.ui.executeWODateInput.text()
+        backCaseDetails = self.ui.executeWOBackCaseDetailsInput.currentText()
+        drillingArrangement = self.ui.executeWODrillingArrangementSelection.currentText()
+        quantity = self.ui.executeWOQuantityInput.text()
+        productionDate = self.ui.executeWOProductionDateInput.text()
+        shippingMethod = self.ui.executeWOShippingMethod.currentText()
+        
+        print("Following work order executed: ")
+        print("Work Order Number: ", workOrderNumber)
+        print("Customer: ", customer)
+        print("Order Date: ", orderDate)
+        print("Back Case Details: ", backCaseDetails)
+        print("Drilling Arrangement: ", drillingArrangement)
+        print("Quantity: ", quantity)
+        print("Production Date: ", productionDate)
+        print("Shipping Method: ", shippingMethod)
+        self.close()
 
 # Widget execution code
 if __name__ == '__main__':
