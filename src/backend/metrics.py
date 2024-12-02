21a3ffb075cd57807d695448ed60f34e69119359
James <jamie.ropotar@gmail.com>
2024-12-02 13:22:39 -0800

    Outlined Implementation of Metrics Calculation

---
+++ src/backend/metrics.py
@@ -0,0 +1,16 @@
+class metrics:
+    
+    def calculateOEE():
+        a = 1
+        #Go through list of workorders to gather all stats
+    
+    def calculateFPFY():
+        return 1 #All product is conforming
+        #can cycle through lists to get this info
+
+    def calculateTSP():
+        return 1 #return total produced units
+    
+    def calculateDownTime():
+        #Return a list, in order, Scheduled downtime (0), Unexpected Downtime (0), Idle time (Real Time - Expected time >= 0), Total (Sum of other elements)
+        return []
