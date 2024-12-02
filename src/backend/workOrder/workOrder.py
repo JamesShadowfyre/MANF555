69e67630a2df324efbd7a8625ca873fb06af520d
James <jamie.ropotar@gmail.com>
2024-12-02 11:49:42 -0800

    Updarte to WorkOrder for Stats

--- src/backend/workOrder/workOrder.py
+++ src/backend/workOrder/workOrder.py
@@ -9,6 +9,7 @@ class WorkOrder:
         self.componentMap = componentMap
         self.quantity = quantity
         #SOMETHING TO SQL HERE
+        self.stats = []
 
     def execute(self): 
         machine: factory.AbstractMachine.AbstractMachine
@@ -16,12 +17,15 @@ class WorkOrder:
         for machine in self.machineList:
             for item in self.componentMap:
                 if item.getMachine() == machine.nameString():
-                    machine.execute(item.getTaskCode(), self.componentMap[self.quantity])
+                    self.stats.append(machine.execute(item.getTaskCode(), self.componentMap[self.quantity]))
 
 
 
     def loadWorkOrderFromDatabase():
         readfromDB = True
+
+    def getStats(self):
+        return self.stats
 
 if __name__ == "__main__":
     myWorkOrder = WorkOrder(1)
