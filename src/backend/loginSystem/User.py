f4f50d009d8654301ad05908dcd526f94ee06841
James <jamie.ropotar@gmail.com>
2024-12-03 13:39:01 -0800

    Updated user

--- src/backend/loginSystem/User.py
+++ src/backend/loginSystem/User.py
@@ -1,25 +1,18 @@
 from backend.externalCommunication.database import Database
 class User:
+    currentUser: str
 
     def verify(username, password):
         db = Database()
         returnedUser = db.select(table='USER', fields=r'username, password', conditions=('username = \'' + username + '\''))
         if (returnedUser.fetchone()[1] == password):
+            
+            User.currentUser = username
+            print(User.getUser())
             return True
         return False
 
-    def getUser (): #called by other classes to return the username 
-        return username
+    def getUser(): #called by other classes to return the username ]
+        return User.currentUser
 
-    def getUserId ():
-        return userId
-
-    def createWorkOrder():
-        pass
-
-    def createProductionSchedule():
-        pass
-    
-    def updateInventory():
-        pass
 
