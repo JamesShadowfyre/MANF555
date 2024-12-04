51115fe12bb4b44fbbe34baa8277829f27e6d16d
James <jamie.ropotar@gmail.com>
2024-12-04 11:07:54 -0800

    Init update

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,5 @@
 from backend.apiAccessPoint import ApplicationHome
-#MainWindow_Handler
 from frontend.PyGuis.LoginWindow_Handler import loginWindowHandler
-# from ProductionScheduleManagerWidget import Ui_productionSchedulerWiget
 from PyQt5 import QtWidgets as qtw
 
 
