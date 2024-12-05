4891bc1a30792a81580ae9c791e21cf0a74554f6
James <jamie.ropotar@gmail.com>
2024-12-05 12:46:18 -0800

    caught divide by zero issue

--- src/backend/metrics.py
+++ src/backend/metrics.py
@@ -2,7 +2,10 @@ class metrics:
     
     def calculateOEE(totalitems, totalWorkTime, totalExpectedDuration):
         availability = 1
-        performance = totalExpectedDuration*totalitems / totalWorkTime
+        try:
+            performance = totalExpectedDuration*totalitems / totalWorkTime
+        except:
+            performance = 0
         quality=1
         return availability*performance*quality
         #Go through list of workorders to gather all stats
