1548ab1db848adb561b88b13e96366ba879e999a
Jon Kettle <jwkettle@student.ubc.ca>
2024-12-06 11:25:15 -0800

    sdf

--- src/frontend/PyGuis/PerformCycleCounts_Handler.py
+++ src/frontend/PyGuis/PerformCycleCounts_Handler.py
@@ -1,5 +1,5 @@
-#from frontend.PyGuis.PerformCycleCounts import Ui_updateInventoryWidget
-from PerformCycleCounts import Ui_updateInventoryWidget
+from frontend.PyGuis.PerformCycleCounts import Ui_updateInventoryWidget
+#from PerformCycleCounts import Ui_updateInventoryWidget
 from PyQt5 import QtWidgets as qtw
 # from PyQt5 import QtGui
 from PyQt5 import QtCore as qtc
