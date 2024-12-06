3f5e0eea9c8120b64bdecc99089a28ab1ca6112e
Morgan <morgandavis239@gmail.com>
2024-12-06 00:39:30 -0800

    PerformCycleCounts_Handler prepared for db

--- src/test2__init__.py
+++ src/test2__init__.py
@@ -1,11 +1,11 @@
 from backend.apiAccessPoint import ApplicationHome
-from frontend.PyGuis.AddNewOperatorWidget_Handler import AddNewOperatorHandler
+from frontend.PyGuis.PerformCycleCounts_Handler import updateInventoryHandler
 from PyQt5 import QtWidgets as qtw
 
 
 api = ApplicationHome()
 api.init_app()
 app = qtw.QApplication([])
-widget = AddNewOperatorHandler()
+widget = updateInventoryHandler()
 widget.show()
 app.exec()
--- src/test4__init__.py
+++ src/test4__init__.py
@@ -1,11 +1,11 @@
 from backend.apiAccessPoint import ApplicationHome
-from frontend.PyGuis.DeleteOperatorWidget_Handler import DeleteOperatorWidgetHandler
+from frontend.PyGuis.EditCustomerWidget_Handler import EditCustomerWidgetHandler
 from PyQt5 import QtWidgets as qtw
 
 
 api = ApplicationHome()
 api.init_app()
 app = qtw.QApplication([])
-widget = DeleteOperatorWidgetHandler()
+widget = EditCustomerWidgetHandler()
 widget.show()
 app.exec()
